abfea977701a439837e4f141478bc3b1701d27ee
Bigetion <adysuprianto@yahoo.co.id>
2021-02-27 20:45:12 +0800

    Update Fix

--- src/layouts/Integral/Navigation.js
+++ src/layouts/Integral/Navigation.js
@@ -1,5 +1,4 @@
 import React, { useState, createContext, useContext } from 'react';
-
 import { classNames } from 'css-hash';
 
 import { navigationClass } from './style';
--- src/layouts/Intuitive/Navigation.js
+++ src/layouts/Intuitive/Navigation.js
@@ -1,6 +1,7 @@
 import React, { useState, createContext, useContext } from 'react';
-
 import { classNames } from 'css-hash';
+
+import Collapse from 'components/Collapse';
 
 import { navigationClass } from './style';
 
@@ -67,15 +68,17 @@ function MultiItem(props) {
         {icon && <i className={icon} />}
         <span>{title}</span>
       </a>
-      {showSubMenu && (
-        <ul>
-          {children.map((item, index) => (
-            <React.Fragment key={index}>
-              <Item {...item} />
-            </React.Fragment>
-          ))}
-        </ul>
-      )}
+      <Collapse open={showSubMenu}>
+        {(collapseProps) => (
+          <ul {...collapseProps}>
+            {children.map((item, index) => (
+              <React.Fragment key={index}>
+                <Item {...item} />
+              </React.Fragment>
+            ))}
+          </ul>
+        )}
+      </Collapse>
     </li>
   );
 }
